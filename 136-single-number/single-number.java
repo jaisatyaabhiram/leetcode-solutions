7f46b5e31c238831609c6c0291b573cf9794676f
Jai Satya Abhiram Mallidi <163704204+jaisatyaabhiram@users.noreply.github.com>
2026-07-30 12:21:38 +0530

    Time: 1 ms (99.96%) | Memory: 47.1 MB (13.56%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        List<Integer> check = new ArrayList<>();
-        for(int i =0; i<nums.length; i++){
-            if(!check.isEmpty()){
-                if(check.contains(nums[i])){
-                    check.remove(check.indexOf(nums[i]));
-                }else{
-                    check.add(nums[i]);
-                }
-            }else{
-                check.add(nums[i]);
-            }
-        }
-        return check.get(0);
+       int n = nums.length;
+       int xor = 0;
+       for(int i = 0;i < n;i ++){
+        xor = xor ^ nums[i];
+       }
+       return xor;
     }
 }
